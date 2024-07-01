ad006c7ac7c436f0e1b3c8608fd0ea467ce7825c
Milan Paulson <milanpaulson8@gmail.com>
2024-07-01 12:50:56 +0530

    sixth stage passed

--- app/main.py
+++ app/main.py
@@ -7,12 +7,21 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
+        known_commands = ["exit", "echo", "type"]
+
         # Wait for user input
         command = input()
-        if command == "exit 0":
-            break
-        elif command.split()[0] == "echo":
-            sys.stdout.write(f"{command[5:]}\n")
+        args = command.split()
+        if args[0] == "exit":
+            if args[1] == "0":
+                break
+        elif args[0] == "echo":
+            sys.stdout.write(" ".join(args[1:]) + "\n")
+        elif args[0] == "type":
+            if args[1] in known_commands:
+                sys.stdout.write(f"{args[1]} is a shell builtin\n")
+            else:
+                sys.stdout.write(f"{args[1]}: not found\n")
         else:
             sys.stdout.write(f"{command}: command not found\n")
 
