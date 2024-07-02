801a6e3b55010a8c4d39483858386eac0b09ce11
Milan Paulson <milanpaulson8@gmail.com>
2024-07-02 13:49:20 +0530

    15th stage passed

--- app/main.py
+++ app/main.py
@@ -17,38 +17,39 @@ def main():
         # print(paths)
         args = command.split()
 
-        if args[0] == "exit":
-            if args[1] == "0":
-                break
-        elif args[0] == "echo":
-            sys.stdout.write(" ".join(args[1:]) + "\n")
-        elif args[0] == "pwd":
-            sys.stdout.write(os.getcwd() + '\n')
-        elif args[0] == "cd":
-            try:
-                path = args[1]
-                if args[1] == '~':
-                    path = home_path
-                os.chdir(path)
-            except:
-                sys.stdout.write(f"cd: {args[1]}: No such file or directory\n")
-        elif args[0] == "type":
-            if args[1] in known_commands:
-                sys.stdout.write(f"{args[1]} is a shell builtin\n")
-            else:
+        match args[0]:
+            case "exit":
+                if args[1] == "0":
+                    break
+            case "echo":
+                sys.stdout.write(" ".join(args[1:]) + "\n")
+            case "pwd":
+                sys.stdout.write(os.getcwd() + '\n')
+            case "cd":
+                try:
+                    path = args[1]
+                    if args[1] == '~':
+                        path = home_path
+                    os.chdir(path)
+                except:
+                    sys.stdout.write(f"cd: {args[1]}: No such file or directory\n")
+            case "type":
+                if args[1] in known_commands:
+                    sys.stdout.write(f"{args[1]} is a shell builtin\n")
+                else:
+                    for path in paths:
+                        if os.path.isdir(path) and args[1] in os.listdir(path):
+                            sys.stdout.write(f"{args[1]} is {path}/{args[1]}\n")
+                            break
+                    else:
+                        sys.stdout.write(f"{args[1]}: not found\n")
+            case '-':
                 for path in paths:
-                    if os.path.isdir(path) and args[1] in os.listdir(path):
-                        sys.stdout.write(f"{args[1]} is {path}/{args[1]}\n")
+                    if os.path.isdir(path) and args[0] in os.listdir(path):
+                        os.system(command)
                         break
                 else:
-                    sys.stdout.write(f"{args[1]}: not found\n")
-        else:
-            for path in paths:
-                if os.path.isdir(path) and args[0] in os.listdir(path):
-                    os.system(command)
-                    break
-            else:
-                sys.stdout.write(f"{command}: command not found\n")
+                    sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
